cfee37095ccad2d76f5c622c12b36b510045a59f
Caleb Bryant <70673772+calebryant@users.noreply.github.com>
2025-05-07 21:53:45 -0500

    Update install-docker-ubuntu.sh

--- scripts/install-docker-ubuntu.sh
+++ scripts/install-docker-ubuntu.sh
@@ -1,3 +1,5 @@
+set -x
+
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
@@ -14,4 +16,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
